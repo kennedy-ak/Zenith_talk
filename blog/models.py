f0870ec739542a9181e39807e6d0a4fba0e80b3a
kennedy-ak <akogokennedy@gmail.com>
2023-12-12 15:21:53 +0000

    changes

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,7 @@ from django.urls import reverse
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager,
-                     self).get_queryset()\
-                          .filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 def post_list(request):
     posts = Post.published.all()
+    posttin  =Post.objects.all()
+    print(len(posttin))
+    print(len(posts))
     return render(request,
                  'blog/post/list.html',
                  {'posts': posts})
