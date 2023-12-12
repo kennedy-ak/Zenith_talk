f618de2a0c0f6277a5152a170506836a1a515598
kennedy-ak <akogokennedy@gmail.com>
2023-12-12 14:42:13 +0000

    creating models managers and cannonical urls

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from django.urls import reverse
+
 
 
 # Create your models here.
@@ -30,9 +32,15 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
-        # ...
+        
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
