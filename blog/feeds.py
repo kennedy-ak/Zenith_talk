a8cc3a2d259be92cc52230e327ceefebc0d55680
kennedy-ak <akogokennedy@gmail.com>
2023-12-21 17:08:18 +0000

    adding feeds

---
+++ blog/feeds.py
@@ -0,0 +1,18 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import  reverse_lazy
+from .models import Post
+
+class LastestPostFeed(Feed):
+    title = 'My blog'
+    link = reverse_lazy('blog:post_list')
+    description = 'New posts of my blog'
+
+    def item(self):
+        return Post.published.all()[:1]
+
+    def item_title(self,item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords(item.body, 30)
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .feeds import LastestPostFeed
+
 app_name = 'blog'
 urlpatterns = [
     # post views
@@ -7,4 +9,5 @@ urlpatterns = [
     # path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>',views.post_detail,name='post_detail'),
     path('<int:post_id>/share/',views.post_share, name='post_share'),
+    path('feed/',LastestPostFeed(),name='post_feed'),
 ]
