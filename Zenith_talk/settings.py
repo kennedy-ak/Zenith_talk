65531ad2a33e76fc161149cd42067f094638673c
kennedy-ak <akogokennedy@gmail.com>
2023-12-21 16:30:02 +0000

    creating sitemaps

--- Zenith_talk/settings.py
+++ Zenith_talk/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
      'blog.apps.BlogConfig',
      'taggit',
     'django.contrib.admin',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -46,6 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
