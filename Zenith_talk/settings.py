8eb1f968c4e3190e58d8fa4e51dbac494b988fff
kennedy-ak <akogokennedy@gmail.com>
2024-09-29 13:27:08 +0000

    css

--- Zenith_talk/settings.py
+++ Zenith_talk/settings.py
@@ -123,7 +123,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
